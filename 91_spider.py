e6289dff5dd3d427086ec9b726ab2ec60d8d8011
snowy920 <36103007+snowy920@users.noreply.github.com>
2018-07-31 18:37:24 +0800

    Update 91_spider.py

--- 91_spider.py
+++ 91_spider.py
@@ -1,29 +1,15 @@
-Skip to content
-￼
-Search or jump to…
-￼
-Pull requests
-Issues
-Marketplace
-Explore
-￼￼ Sign out
- Watch 0
-￼ Star 0  Fork 72 snowy920/91_porn_spider
-forked from eqblog/91_porn_spider
- Code  Pull requests 0  Projects 0  Wiki  Insights  Settings
-￼Branch: master Find file Copy path 91_porn_spider/91_spider.py
-2535f0a  3 minutes ago
-￼ snowy920 Update 91_spider.py
-2 contributors￼￼
-RawBlameHistory  ￼  ￼
-49 lines (48 sloc)  2.3 KB
 import requests
 import os,re,time,random
 def download_mp4(url,dir):
     headers={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36Name','Referer':'http://91porn.com'}
     req=requests.get(url=url)
-    filename='/'+str(dir)+'.mp4'
+    filename=str(dir)+'/1.mp4'
     with open(filename,'wb') as f:
+        f.write(req.content)
+def download_img(url,dir):
+    headers={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36Name','Referer':'http://91porn.com'}
+    req=requests.get(url=url)
+    with open(str(dir)+'/thumb.png','wb') as f:
         f.write(req.content)
 def random_ip():
     a=random.randint(1,255)
@@ -56,6 +42,7 @@ while flag<=100:
             try:
                 os.makedirs(str(t))
                 print('开始下载:'+str(t))
+                download_img(str(img_url[0]),str(t))
                 download_mp4(str(video_url[0]),str(t))
                 print('下载完成')
             except:
@@ -65,16 +52,3 @@ while flag<=100:
             time.sleep(2)
     flag=flag+1
     print('此页已下载完成，下一页是'+str(flag))
-© 2018 GitHub, Inc.
-Terms
-Privacy
-Security
-Status
-Help
-Contact GitHub
-API
-Training
-Shop
-Blog
-About
-Press h to open a hovercard with more details.
