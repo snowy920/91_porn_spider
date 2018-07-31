2535f0aa09489662b1126d7e1428910ad7788bdc
snowy920 <36103007+snowy920@users.noreply.github.com>
2018-07-31 18:33:33 +0800

    Update 91_spider.py
    取消下载到文件夹，取消下载图片，获取Tittle为MP4名称

--- 91_spider.py
+++ 91_spider.py
@@ -3,13 +3,8 @@ import os,re,time,random
 def download_mp4(url,dir):
     headers={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36Name','Referer':'http://91porn.com'}
     req=requests.get(url=url)
-    filename=str(dir)+'/1.mp4'
+    filename='/'+str(dir)+'.mp4'
     with open(filename,'wb') as f:
-        f.write(req.content)
-def download_img(url,dir):
-    headers={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36Name','Referer':'http://91porn.com'}
-    req=requests.get(url=url)
-    with open(str(dir)+'/thumb.png','wb') as f:
         f.write(req.content)
 def random_ip():
     a=random.randint(1,255)
@@ -42,7 +37,6 @@ while flag<=100:
             try:
                 os.makedirs(str(t))
                 print('开始下载:'+str(t))
-                download_img(str(img_url[0]),str(t))
                 download_mp4(str(video_url[0]),str(t))
                 print('下载完成')
             except:
